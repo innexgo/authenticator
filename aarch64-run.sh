e64e79789747ceeb52a340f2523e86e40872f092
gpi <gpimpale29@gmail.com>
2022-04-14 16:35:04 -0700

    do changes

--- aarch64-run.sh
+++ aarch64-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./aarch64/auth-service \
+exec ./aarch64/auth-service \
   --port=8079 \
   --database-url=postgres://ubuntu:toor@localhost/auth \
-  --site-external-url=critica.ucla.edu \
+  --site-external-url=https://critica.eaucla.org \
   --mail-service-url=http://localhost:8078
